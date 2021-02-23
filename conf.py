6c3530aa357989696690d6a845d969db12c5ad51
pranathi1210 <79441140+pranathi1210@users.noreply.github.com>
2021-02-23 16:46:25 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'git and github'
+copyright = '2021, 'pranathi'
+author = 'pranathi'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
